fa2f2fc5eec58c463397147080213a0b280c8c0f
Utkarsha Gurjar <ugurjar@uncc.edu>
2020-06-19 09:30:37 -0400

    Adding comments and rating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatSliderModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -64,7 +65,8 @@ import { LoginComponent } from './login/login.component';
     MatSelectModule,
     MatSlideToggleModule,
     ReactiveFormsModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatSliderModule
   ],
   
   providers: [DishService, 
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { Location } from '@angular/common';
 import { Params, ActivatedRoute } from '@angular/router';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
+import { Comment } from '../shared/comment';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {MatSliderModule, MatFormFieldModule } from '@angular/material';
 
 @Component({
   selector: 'app-dishdetail',
@@ -16,10 +19,33 @@ export class DishdetailComponent implements OnInit {
   dishIds: string[];
   prev: string;
   next: string;
+  ratingForm: FormGroup;
+  comment: Comment;
+  dishcopy = null;
+
+  formErrors = {
+    'author': '',
+    'comment': '',
+  };
+
+  validationMessages = {
+    'author': {
+      'required':      'Author is required.',
+      'minlength':     'Author must be at least 2 characters long.',
+      'maxlength':     'Author cannot be more than 25 characters long.'
+    },
+    'comment': {
+      'required':      'Comment is required.',
+      'minlength':     'Author must be at least 2 characters long.'
+    },
+  };
 
   constructor(private dishService: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) { 
+      this.createForm();
+    }
 
     ngOnInit() {
       this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
@@ -27,6 +53,42 @@ export class DishdetailComponent implements OnInit {
       .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
     }
   
+    createForm() : void {
+      this.ratingForm = this.fb.group({
+        author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+        comment: ['', [Validators.required, Validators.minLength(2)] ],
+        rating: 5
+      });
+    
+  
+    this.ratingForm.valueChanges
+        .subscribe(data => this.onValueChanged(data));
+  
+      this.onValueChanged(); // (re)set validation messages now
+  
+    }
+
+    onValueChanged(data?: any) {
+      if (!this.ratingForm) { return; }
+      const form = this.ratingForm;
+      for (const field in this.formErrors) {
+        if (this.formErrors.hasOwnProperty(field)) {
+          // clear previous error message (if any)
+          this.formErrors[field] = '';
+          const control = form.get(field);
+          if (control && control.dirty && !control.valid) {
+            const messages = this.validationMessages[field];
+            for (const key in control.errors) {
+              if (control.errors.hasOwnProperty(key)) {
+                this.formErrors[field] += messages[key] + ' ';
+              }
+            }
+          }
+        }
+      }
+    }
+
+
     setPrevNext(dishId: string) {
       const index = this.dishIds.indexOf(dishId);
       this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
@@ -36,4 +98,20 @@ export class DishdetailComponent implements OnInit {
   goBack():void {
       this.location.back();
   }
+
+  onSubmit() {
+    this.comment = this.ratingForm.value;
+     this.comment.date = new Date().toISOString();
+     this.dishcopy.comments.push(this.comment);
+     this.dishcopy.save()
+      .subscribe(dish => { this.dish = dish; console.log(this.dish); });
+       console.log(this.comment);
+       this.dish.comment.push(this.comment);
+       this.ratingForm.reset({
+        author: '',
+        comment: '',
+        rating: 5
+    });
+  }
+
 }
